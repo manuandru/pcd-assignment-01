7732a4b0de53d59d94b453ef263715567af58ee5
Manuel Andruccioli <manuel.andruccioli@studio.unibo.it>
2023-04-08 10:06:43 +0200

    update concurrent test

--- part-01/src/pcd/assignment01/model/TestConcurrentRead.java
+++ part-01/src/pcd/assignment01/model/TestConcurrentRead.java
@@ -9,18 +9,27 @@ import java.util.concurrent.CyclicBarrier;
 public class TestConcurrentRead {
     public static void main(String[] args) throws InterruptedException {
 
-        CyclicBarrier barrier = new CyclicBarrier(2);
+        CyclicBarrier barrier = new CyclicBarrier(5);
 
-        Thread reader1 = new WorkerReader(barrier, "data/file.java");
-        Thread reader2 = new WorkerReader(barrier, "data/file2");
+        Thread reader1 = new WorkerReader(barrier, "data/file3.java");
+        Thread reader2 = new WorkerReader(barrier, "data/file3.java");
+        Thread reader3 = new WorkerReader(barrier, "data/file3.java");
+        Thread reader4 = new WorkerReader(barrier, "data/file3.java");
+        Thread reader5 = new WorkerReader(barrier, "data/file3.java");
 
-        Thread.sleep(10000);
         long start = System.currentTimeMillis();
         reader1.start();
         reader2.start();
+        reader3.start();
+        reader4.start();
+        Thread.sleep(10000);
+        reader5.start();
 
         reader1.join();
         reader2.join();
+        reader3.join();
+        reader4.join();
+        reader5.join();
         long stop = System.currentTimeMillis();
         System.out.println("Time: " + (stop - start));
 
